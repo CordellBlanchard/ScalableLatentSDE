2ef92eafc848e8da9a4236beb9ae4ee24d64cd5d
kareemalsawah <alsawahkareem@gmail.com>
2023-11-21 11:19:18 -0500

    Added new synthetic dataset import

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,4 +1,4 @@
-from .synthetic import linear_gssm, nonlinear_gssm
+from .synthetic import linear_gssm, nonlinear_gssm, nonlinear_gssm2
 from .synthetic_pcd import linear_synthetic_pcd
 from .jsb_piano import jsb_piano
 from .physionet import physionet
